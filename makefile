2843125b40f28e1688c7ee4ab8a4db4f7ddc0767
Oz Elentok <oz.elen@gmail.com>
2016-01-14 21:57:41 +0200

    Change naming conventions from camelCase to underscore_case

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 CC = g++
 FLAGS = -Wall -Wextra -Wconversion -Wsign-conversion -Wformat-security -pie -fPIE -Wl,-z,relro,-z,now -Werror -O2 -std=c++11
 
-all: zprompt 
+all: zprompt
 zprompt: zprompt.cpp
 	$(CC) $(FLAGS) $^ -o $@
 	strip $@
--- zprompt.cpp
+++ zprompt.cpp
@@ -8,107 +8,107 @@
 using std::string;
 using std::stringstream;
 
-static const string orgSeperator("/");
-static const string seperator("⮀");
-static const string thinSep(" ⮁ ");
-static const string homeStart(" ~");
-static const string promptStart("\n $ ");
-static const string colorTemplateBG("%{[48;5;m%}");
-static const string colorTemplateFG("%{[38;5;m%}");
-static const string resetColors("%{[0m%}");
-static const char * const homeEnvironment = "HOME";
-static const size_t colorInsertionIndex = 9;
-static const size_t averagePromptSize = 150;
+static const string UNIX_SEPERATOR("/");
+static const string ARROW_SEPERATOR("⮀");
+static const string THIN_ARROW_SEPERATOR(" ⮁ ");
+static const string CWD_HOME(" ~");
+static const string PROMPT_START("\n $ ");
+static const string BACKGROUND_COLOR_TEMPLATE("%{[48;5;m%}");
+static const string FOREGROUND_COLOR_TEMPLATE("%{[38;5;m%}");
+static const string COLORS_STOP("%{[0m%}");
+static const char* const HOME_ENVIRONMENT_VARIABLE_NAME = "HOME";
+static const size_t COLOR_INSERTION_INDEX = 9;
+static const size_t DEFAULT_PROMPT_SIZE = 150;
 
 namespace colors
 {
-	static const string pathFG("255");
-	static const string pathBG("25");
-	static const string promptBG("22");
+	static const string CWD_FOREGROUND("255");
+	static const string CWD_BACKGROUND("25");
+	static const string PROMPT_BACKGROUND("22");
 }
 
-string startColorFG(const string & fgColor)
+string start_color(const string& color_starter_template, const string& color)
 {
-	string fgText(colorTemplateFG);
-	fgText.insert(colorInsertionIndex,fgColor);
-	return fgText;
+	string color_starter(color_starter_template);
+	color_starter.insert(COLOR_INSERTION_INDEX, color);
+	return color_starter;
 }
 
-string startColorBG(const string & bgColor)
+string start_foreground_color(const string& color)
 {
-	string bgText(colorTemplateBG);
-	bgText.insert(colorInsertionIndex, bgColor);
-	return bgText;
+	return start_color(FOREGROUND_COLOR_TEMPLATE, color);
 }
 
-void replaceSeperators(string & cwd)
+string start_background_color(const string& color)
+{
+	return start_color(BACKGROUND_COLOR_TEMPLATE, color);
+}
+
+void replace_seperators(string& cwd)
 {
 	size_t index = 0;
 	bool is_root = false;
-	is_root = (orgSeperator.length() == cwd.length());
+	is_root = UNIX_SEPERATOR == cwd;
 	while (true) {
-		index = cwd.find(orgSeperator, index);
+		index = cwd.find(UNIX_SEPERATOR, index);
 		if (index == std::string::npos) {
 			break;
 		}
-		cwd.replace(index, orgSeperator.length(), thinSep);
-		index += thinSep.length();
+		cwd.replace(index, UNIX_SEPERATOR.length(), THIN_ARROW_SEPERATOR);
+		index += THIN_ARROW_SEPERATOR.length();
 	}
 	if (!is_root) {
 		cwd.append(" ");
 	}
 }
 
-void replaceHomePath(string & cwd)
+void replace_home_path(string& cwd)
 {
-	char * homeCString = getenv(homeEnvironment);
-	string home;
-	size_t index = 0;
-	if (homeCString == nullptr) {
+	const char* const raw_home = getenv(HOME_ENVIRONMENT_VARIABLE_NAME);
+	if (raw_home == nullptr) {
 		return;
 	}
-	home = homeCString;
-	index = cwd.find(home, index);
+	string home(raw_home);
+	size_t index = cwd.find(home, index);
 	if (index == 0) {
-		cwd.replace(index, home.length(), homeStart);
+		cwd.replace(index, home.length(), CWD_HOME);
 	}
 }
 
-void addCurrentWorkingDirectory(string & shellPrompt)
+void append_current_working_directory(string& prompt)
 {
-	char * cwdCString = getenv("PWD");
-	string cwd;
-	if (cwdCString == nullptr) {
+	const char* raw_cwd = getenv("PWD");
+	if (raw_cwd == nullptr) {
 		return;
 	}
-	cwd = cwdCString;
-	replaceHomePath(cwd);
-	replaceSeperators(cwd);
-	shellPrompt += startColorFG(colors::pathFG);
-	shellPrompt += startColorBG(colors::pathBG);
-	shellPrompt += cwd;
-	shellPrompt += resetColors;
-	shellPrompt += startColorFG(colors::pathBG);
-	shellPrompt += seperator;
-	shellPrompt += resetColors;
+	string cwd = raw_cwd;
+	replace_home_path(cwd);
+	replace_seperators(cwd);
+	prompt += start_foreground_color(colors::CWD_FOREGROUND);
+	prompt += start_background_color(colors::CWD_BACKGROUND);
+	prompt += cwd;
+	prompt += COLORS_STOP;
+	prompt += start_foreground_color(colors::CWD_BACKGROUND);
+	prompt += ARROW_SEPERATOR;
+	prompt += COLORS_STOP;
 }
 
-void addPrompt(string & shellPrompt)
+void append_prompt_line(string& prompt)
 {
-	shellPrompt += startColorBG(colors::promptBG);
-	shellPrompt += promptStart;
-	shellPrompt += resetColors;
-	shellPrompt += startColorFG(colors::promptBG);
-	shellPrompt += seperator;
-	shellPrompt += resetColors;
+	prompt += start_background_color(colors::PROMPT_BACKGROUND);
+	prompt += PROMPT_START;
+	prompt += COLORS_STOP;
+	prompt += start_foreground_color(colors::PROMPT_BACKGROUND);
+	prompt += ARROW_SEPERATOR;
+	prompt += COLORS_STOP;
 }
 
 int main()
 {
-	string shellPrompt;
-	shellPrompt.reserve(averagePromptSize);
-	addCurrentWorkingDirectory(shellPrompt);
-	addPrompt(shellPrompt);
-	std::cout << shellPrompt;
+	string prompt;
+	prompt.reserve(DEFAULT_PROMPT_SIZE);
+	append_current_working_directory(prompt);
+	append_prompt_line(prompt);
+	std::cout << prompt;
 	return 0;
 }
